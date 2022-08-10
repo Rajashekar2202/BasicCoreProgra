0214af83b8a34b273917806e573ec1f5e22c29f9
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-10 23:00:24 +0530

    added VowelConsonent program

---
+++ VowelConsonent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class VowelConsonent
+    {
+        public static void VowelOrConsonent()
+        {
+            
+            Console.WriteLine("Enter a Letter");
+            char c = Convert.ToChar(Console.Read());
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' )
+            {
+                Console.WriteLine(c + " is Vowel");
+            }
+            else
+            {
+                Console.WriteLine(c + " is Consonent");
+            }
+
+        }
+    }
+}
