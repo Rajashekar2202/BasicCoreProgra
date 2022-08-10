060bfd85cd1106e3c513c29c0bba7950ac959535
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-10 22:49:37 +0530

    added LeapYear program

---
+++ LeapYear.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class LeapYear
+    {
+        public static void CheckLeapYear()
+        {
+            int year;
+            Console.WriteLine("Enter the year");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 ==0) && (year % 100 !=0)) || (year % 400 ==0))
+            {
+                Console.WriteLine("Given year is Leap year");
+            }
+            else
+            {
+                Console.WriteLine("Given year is not a Leap year");
+            }
+
+
+
+        }
+    }
+}
