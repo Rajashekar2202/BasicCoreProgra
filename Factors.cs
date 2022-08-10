c582534dcc1b986fe82aad9e43765453ea08549e
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-10 22:37:20 +0530

    added Factors program

---
+++ Factors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class Factors
+    {
+        public static void PrimeFactors()
+        {
+            Console.WriteLine("Enter the Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int value;   
+            Console.WriteLine("Factors of " + num + " is " );
+            for (value = 2; value * value <= num; value++)
+            {
+                while(num % value == 0)
+                {
+                    Console.WriteLine(value + ",");
+                    num = num / value;
+                }
+            }
+        }
+    }
+}
