f9ff1dc5bb3287d42165065a31be812231b46182
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-10 22:57:47 +0530

    added SwapTwoNumbers program

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class SwapTwoNumbers
+    {
+        public static void SwapTheTwoNumbers()
+        {
+            Console.Write("Enter Number1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Number2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c;
+
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("After swapping Num1: " + a);
+            Console.WriteLine("After swapping Num2: " + b);
+
+        }
+    }
+}
